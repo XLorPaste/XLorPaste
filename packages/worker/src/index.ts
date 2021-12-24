847bfd7ee5f7a3eda8c9056ffda3401012348334
XLor <yjl9903@vip.qq.com>
2021-12-24 15:41:02 +0800

    chore: fmt

--- packages/worker/src/index.ts
+++ packages/worker/src/index.ts
@@ -6,6 +6,10 @@ worker.get('/', async (req: Request) => {
   return 'Hello, this is XLorPaste Workers API!'
 });
 
+worker.post('/', async (req: Request) => {
+  
+});
+
 addEventListener('fetch', event => {
   event.respondWith(worker.handle(event.request))
 });
