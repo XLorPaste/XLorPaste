ec974b541d0bfcafe8bcb1a65f3e80f87caea93b
XLor <yjl9903@vip.qq.com>
2022-05-07 02:17:04 +0800

    fix: storage list

--- packages/worker/env.d.ts
+++ packages/worker/env.d.ts
@@ -1,3 +1,5 @@
 declare const __DEV__: boolean;
 
 declare const ADMIN_KEY: string;
+
+declare const XLORPASTE: KVNamespace;
--- packages/worker/src/storage.ts
+++ packages/worker/src/storage.ts
@@ -1,16 +1,7 @@
-import { createStorage, prefixStorage, Storage } from 'unstorage';
-import cloudflareKVDriver from 'unstorage/drivers/cloudflare-kv';
+import { createStorage, prefixStorage, Storage, defineDriver } from 'unstorage';
 import memoryDriver from 'unstorage/drivers/memory';
 
 import { randomString } from './utils';
-
-export const storage = createStorage({
-  driver: __DEV__ ? memoryDriver() : cloudflareKVDriver({ binding: 'XLORPASTE' })
-});
-
-export const subStore = prefixStorage(storage, 'sub');
-
-export const delStore = prefixStorage(storage, 'del');
 
 export async function genToken(store: Storage, len?: number) {
   let token = randomString(len);
@@ -19,3 +10,41 @@ export async function genToken(store: Storage, len?: number) {
   }
   return token;
 }
+
+const cloudflareKVDriver = defineDriver((opts?: { binding: KVNamespace }) => {
+  const binding = opts?.binding!;
+
+  async function getKeys(base?: string) {
+    const kvList = await binding.list({ prefix: base });
+    return kvList.keys.map((key: any) => key.name);
+  }
+
+  return {
+    async hasItem(key) {
+      return (await binding.get(key)) !== null;
+    },
+    getItem(key) {
+      return binding.get(key);
+    },
+    setItem(key, value) {
+      return binding.put(key, value);
+    },
+    removeItem(key) {
+      return binding.delete(key);
+    },
+    // TODO: use this.getKeys once core is fixed
+    getKeys,
+    async clear() {
+      const keys = await getKeys();
+      await Promise.all(keys.map((key: any) => binding.delete(key)));
+    }
+  };
+});
+
+export const storage = createStorage({
+  driver: __DEV__ ? memoryDriver() : cloudflareKVDriver({ binding: XLORPASTE })
+});
+
+export const subStore = prefixStorage(storage, 'sub');
+
+export const delStore = prefixStorage(storage, 'del');
