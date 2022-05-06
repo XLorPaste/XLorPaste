cefdd7361a848d7d41a034807387da801b064296
XLor <yjl9903@vip.qq.com>
2022-05-07 00:06:07 +0800

    fix: allow header

--- packages/worker/middleware/cors.ts
+++ packages/worker/middleware/cors.ts
@@ -6,11 +6,15 @@ export default defineEventHandler((event) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json'
-  );
-  if (method === 'OPTIONS') {
-    return '';
+  res.setHeader('Access-Control-Max-Age', '86400');
+  if (
+    event.req.headers['Access-Control-Request-Method'] !== null &&
+    event.req.headers['Access-Control-Request-Headers'] !== null
+  ) {
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      event.req.headers.headers['Access-Control-Request-Headers']
+    );
   }
+  if (method === 'OPTIONS') return '';
 });
