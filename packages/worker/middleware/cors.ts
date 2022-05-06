8188247819f93dd10a6fc970edf7000e290d72ea
XLor <yjl9903@vip.qq.com>
2022-05-07 00:13:16 +0800

    fix: cors

--- packages/worker/middleware/cors.ts
+++ packages/worker/middleware/cors.ts
@@ -1,19 +1,19 @@
 import { defineEventHandler, useMethod } from 'h3';
 
 export default defineEventHandler((event) => {
-  const method = useMethod(event);
   const res = event.res;
+  const method = useMethod(event);
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.setHeader('Access-Control-Max-Age', '86400');
+  res.setHeader('Vary', 'Origin');
   if (
-    event.req.headers['Access-Control-Request-Method'] !== null &&
-    event.req.headers['Access-Control-Request-Headers'] !== null
+    !!event.req.headers['Access-Control-Request-Method'] &&
+    !!event.req.headers['Access-Control-Request-Headers']
   ) {
     res.setHeader(
       'Access-Control-Allow-Headers',
-      event.req.headers.headers['Access-Control-Request-Headers']
+      event.req.headers['Access-Control-Request-Headers']
     );
   }
   if (method === 'OPTIONS') return '';
