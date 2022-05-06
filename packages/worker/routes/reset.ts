58c7137f403ed600da722a949ce9f25d0d84f6a9
XLor <yjl9903@vip.qq.com>
2022-05-07 00:48:07 +0800

    feat: reset

---
+++ packages/worker/routes/reset.ts
@@ -0,0 +1,15 @@
+import { defineEventHandler } from 'h3';
+
+import { storage, subStore, delStore } from '~/storage';
+
+export default defineEventHandler(async () => {
+  const keys = await storage.getKeys();
+  console.log(keys);
+  for (const key of keys) {
+    if (key.startsWith('sub/')) {
+      await subStore.setItem(key.slice(4), await storage.getItem(key));
+    } else if (key.startsWith('del/')) {
+      await delStore.setItem(key.slice(4), await storage.getItem(key));
+    }
+  }
+});
